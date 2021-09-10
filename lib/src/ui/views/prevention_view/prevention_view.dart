1da764b15a36de534058d979d1deffe4a691a1fd
31Carlton7 <carltonaikins7@gmail.com>
2021-09-10 15:57:28 -0400

    Create prevention_view.dart

--- lib/src/ui/views/prevention_view/prevention_view.dart
+++ lib/src/ui/views/prevention_view/prevention_view.dart
@@ -16,6 +16,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 import 'package:canton_design_system/canton_design_system.dart';
+import 'package:corona_spectator/src/providers/prevention_data_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:corona_spectator/src/ui/views/prevention_view/components/prevention_view_header.dart';
 
 class PreventionView extends StatelessWidget {
@@ -23,7 +25,16 @@ class PreventionView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color _canvasColor() {
+      if (MediaQuery.of(context).platformBrightness == Brightness.dark) {
+        return Theme.of(context).canvasColor;
+      }
+      return Theme.of(context).backgroundColor;
+    }
+
     return CantonScaffold(
+      padding: EdgeInsets.zero,
+      backgroundColor: _canvasColor(),
       body: _content(context),
     );
   }
@@ -32,6 +43,88 @@ class PreventionView extends StatelessWidget {
     return Column(
       children: [
         PreventionViewHeader(),
+        _body(context),
+      ],
+    );
+  }
+
+  Widget _body(BuildContext context) {
+    var items = context.read(preventionDataProvider);
+    return Expanded(
+      child: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [
+              _preventionCard(
+                context,
+                items[index].title,
+                items[index].content,
+                items[index].iconPath,
+              ),
+              if (index == items.length - 1) Divider(),
+            ],
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _preventionCard(
+      BuildContext context, String title, String content, String iconPath) {
+    return Column(
+      children: [
+        Divider(),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 17, vertical: 10),
+          child: Row(
+            children: [
+              Stack(alignment: Alignment.center, children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                Container(
+                  height: 30,
+                  width: 30,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    shape: BoxShape.circle,
+                  ),
+                  child: SvgPicture.asset(
+                    iconPath,
+                    color: CantonColors.white,
+                  ),
+                ),
+              ]),
+              const SizedBox(width: 12),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: Theme.of(context).textTheme.headline5,
+                    ),
+                    const SizedBox(height: 5),
+                    Text(
+                      content,
+                      style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                            color:
+                                Theme.of(context).colorScheme.secondaryVariant,
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
