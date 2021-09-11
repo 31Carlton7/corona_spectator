d0ed00f9301e2a8fe58a0471a46ce08e4a540fa1
31Carlton7 <carltonaikins7@gmail.com>
2021-09-10 21:51:37 -0400

    Create symptom_card.dart

---
+++ lib/src/ui/views/symptoms_view/components/symptom_card.dart
@@ -0,0 +1,69 @@
+/*
+Copyright (C) 2021  Carlton Aikins
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'package:canton_design_system/canton_design_system.dart';
+
+class SymptomCard extends StatelessWidget {
+  const SymptomCard({required this.text, required this.iconPath});
+
+  final String text;
+  final String iconPath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Divider(),
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 17, vertical: 10),
+          child: Row(
+            children: [
+              Stack(alignment: Alignment.center, children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                Container(
+                  height: 30,
+                  width: 30,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    shape: BoxShape.circle,
+                  ),
+                  child: SvgPicture.asset(
+                    iconPath,
+                    color: CantonColors.white,
+                  ),
+                ),
+              ]),
+              const SizedBox(width: 12),
+              Text(
+                text,
+                style: Theme.of(context).textTheme.headline5,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
