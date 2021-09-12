a65b28b3c53891ebb00444e1e7cb257e5dffb40b
31Carlton7 <carltonaikins7@gmail.com>
2021-09-11 21:57:09 -0400

    Update article_list.dart

--- lib/src/ui/views/news_view/components/article_list.dart
+++ lib/src/ui/views/news_view/components/article_list.dart
@@ -17,8 +17,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import 'package:canton_design_system/canton_design_system.dart';
 import 'package:corona_spectator/src/models/article.dart';
-import 'package:corona_spectator/src/ui/views/news_view/components/article_card_large.dart';
-import 'package:corona_spectator/src/ui/views/news_view/components/article_card_small.dart';
+import 'package:corona_spectator/src/ui/views/news_view/components/article_card.dart';
 
 class ArticleList extends StatelessWidget {
   const ArticleList(this.articles);
@@ -30,14 +29,13 @@ class ArticleList extends StatelessWidget {
     return SliverList(
       delegate: SliverChildBuilderDelegate(
         (context, index) {
-          switch (index) {
-            case 0:
-              return ArticleCardLarge(articles[index]);
-            case 1:
-              return SizedBox(height: 15);
-            default:
-              return ArticleCardSmall(articles[index]);
-          }
+          return Column(
+            children: [
+              Divider(),
+              ArticleCard(articles[index]),
+              if (index == articles.length - 1) Divider(),
+            ],
+          );
         },
         childCount: articles.length,
       ),
