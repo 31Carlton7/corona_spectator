c0fabfa35cdf85802d5878dbc5862a3e4e1a228d
31Carlton7 <carltonaikins7@gmail.com>
2021-09-11 21:52:54 -0400

    Update prevention_repository.dart

--- lib/src/repositories/prevention_repository.dart
+++ lib/src/repositories/prevention_repository.dart
@@ -16,49 +16,46 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 import 'package:corona_spectator/src/models/prevention_data.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class PreventionRepository {
   final List<PreventionData> _data = [
     PreventionData(
       title: 'Stay Home',
-      content:
-          'Try to stay home and quarantine as much as possible to minimize risk of contracting the virus.',
-      iconPath: 'assets/icons/home.svg',
+      content: 'Try to stay home and quarantine as much as possible to minimize risk of contracting the virus.',
+      iconPath: FontAwesomeIcons.home,
     ),
     PreventionData(
       title: 'Get Vaccinated',
       content:
           'Getting a vaccine significantly decreases the chances of contracting COVID-19 and helps to fight the virus.',
-      iconPath: 'assets/icons/vaccine.svg',
+      iconPath: FontAwesomeIcons.syringe,
     ),
     PreventionData(
       title: 'Wash your hands frequently',
       content:
           'Washing your hands removes many germs. Make sure to clean your devices and other things you use consistently as well.',
-      iconPath: 'assets/icons/washing_hands.svg',
+      iconPath: FontAwesomeIcons.handsWash,
     ),
     PreventionData(
       title: 'Wear your mask',
-      content:
-          'Make sure you wear your mask if you go out and are near people, and don\'t let it slip past your nose.',
-      iconPath: 'assets/icons/person_wearing_mask.svg',
+      content: 'Make sure you wear your mask if you go out and are near people, and don\'t let it slip past your nose.',
+      iconPath: FontAwesomeIcons.headSideMask,
     ),
     PreventionData(
       title: 'Maintain Social Distancing',
-      content:
-          'Stay at least 2 meters or 6 feet away from people who show symptoms of the virus.',
-      iconPath: 'assets/icons/social_distancing.svg',
+      content: 'Stay at least 2 meters or 6 feet away from people who show symptoms of the virus.',
+      iconPath: FontAwesomeIcons.peopleArrows,
     ),
     PreventionData(
       title: 'Limit in person interactions',
       content: 'Do not converse with large groups of people.',
-      iconPath: 'assets/icons/interactions.svg',
+      iconPath: FontAwesomeIcons.usersSlash,
     ),
     PreventionData(
       title: 'Stay Calm',
-      content:
-          'Do not worry or panic, and make sure to eat and get enough sleep everyday.',
-      iconPath: 'assets/icons/calm.svg',
+      content: 'Do not worry or panic, and make sure to eat and get enough sleep everyday.',
+      iconPath: FontAwesomeIcons.pray,
     ),
   ];
 
