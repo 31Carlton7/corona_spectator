db1564aea1418461db5bdf89425d76da20d359c3
31Carlton7 <carltonaikins7@gmail.com>
2021-09-11 21:56:12 -0400

    Update who_card.dart

--- lib/src/ui/views/information_view/components/who_card.dart
+++ lib/src/ui/views/information_view/components/who_card.dart
@@ -24,38 +24,52 @@ class WHOCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 12,
-          vertical: 7,
+    final link = 'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public';
+    return GestureDetector(
+      onTap: () async {
+        if (await canLaunch(link)) {
+          await launch(link);
+        } else {
+          throw 'Could not launch $link';
+        }
+      },
+      child: Card(
+        shape: SquircleBorder(
+          radius: BorderRadius.vertical(
+            bottom: Radius.circular(35),
+          ),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Coronavirus disease (COVID-19) advice for the public',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-            const SizedBox(height: 7),
-            Linkify(
-              onOpen: (link) async {
-                if (await canLaunch(link.url)) {
-                  await launch(link.url);
-                } else {
-                  throw 'Could not launch $link';
-                }
-              },
-              options: LinkifyOptions(removeWww: true),
-              text:
-                  'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public',
-              style: Theme.of(context).textTheme.headline6,
-              linkStyle: Theme.of(context).textTheme.headline6?.copyWith(
-                    color: Theme.of(context).colorScheme.onPrimary,
-                    decoration: TextDecoration.underline,
-                  ),
-            ),
-          ],
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 12,
+            vertical: 7,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'World Health Organization',
+                style: Theme.of(context).textTheme.headline6,
+              ),
+              const SizedBox(height: 7),
+              Linkify(
+                onOpen: (link) async {
+                  if (await canLaunch(link.url)) {
+                    await launch(link.url);
+                  } else {
+                    throw 'Could not launch $link';
+                  }
+                },
+                options: LinkifyOptions(removeWww: true),
+                text: 'https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public',
+                style: Theme.of(context).textTheme.headline6,
+                linkStyle: Theme.of(context).textTheme.headline6?.copyWith(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      decoration: TextDecoration.underline,
+                    ),
+              ),
+            ],
+          ),
         ),
       ),
     );
