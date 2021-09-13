c44efa3e653538932569735f5aae062b6380b782
31Carlton7 <carltonaikins7@gmail.com>
2021-09-12 22:34:03 -0400

    Update bottom_naviagtion_bar.dart

--- lib/src/config/bottom_navigation_bar.dart
+++ lib/src/config/bottom_navigation_bar.dart
@@ -16,6 +16,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 import 'package:canton_design_system/canton_design_system.dart';
+import 'dart:math' as math;
 import 'package:flutter/cupertino.dart';
 
 class BottomNavBar extends StatefulWidget {
@@ -31,37 +32,37 @@ class _BottomNavBarState extends State<BottomNavBar> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        border: Border(
-          top: BorderSide(
-            color: Theme.of(context).dividerColor,
-            width: 0.5,
-          ),
-        ),
-      ),
       child: BottomNavigationBar(
         currentIndex: widget.currentIndex,
         onTap: widget.onTabTapped,
-        selectedItemColor: Theme.of(context).primaryColor,
+        backgroundColor: Theme.of(context).colorScheme.onSecondary,
+        selectedItemColor: Theme.of(context).colorScheme.primary,
         unselectedItemColor: Theme.of(context).colorScheme.secondaryVariant,
+        selectedIconTheme: IconThemeData(color: Theme.of(context).primaryColor, size: 24),
         items: [
           BottomNavigationBarItem(
-            label: 'Home',
+            label: 'Stats',
             tooltip: '',
-            activeIcon: Icon(Iconsax.home, size: 24),
-            icon: Icon(Iconsax.home, size: 24),
+            activeIcon: Icon(Iconsax.graph, size: 24),
+            icon: Icon(Iconsax.graph, size: 24),
           ),
           BottomNavigationBarItem(
             label: 'News',
             tooltip: '',
-            activeIcon: Icon(Iconsax.book, size: 24),
-            icon: Icon(Iconsax.book, size: 24),
+            activeIcon: Icon(Iconsax.note_2, size: 24),
+            icon: Icon(Iconsax.note_2, size: 24),
           ),
           BottomNavigationBarItem(
             label: 'Info',
             tooltip: '',
-            activeIcon: Icon(Iconsax.info_circle, size: 24),
-            icon: Icon(Iconsax.info_circle, size: 24),
+            activeIcon: Transform.rotate(
+              angle: math.pi,
+              child: Icon(Iconsax.info_circle, size: 24),
+            ),
+            icon: Transform.rotate(
+              angle: math.pi,
+              child: Icon(Iconsax.info_circle, size: 24),
+            ),
           ),
         ],
       ),
