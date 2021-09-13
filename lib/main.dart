526f5c006998a71b701f2af244b5898f32241008
31Carlton7 <carltonaikins7@gmail.com>
2021-09-12 22:33:47 -0400

    Add more enchanced firebase crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import 'dart:async';
+
 import 'package:canton_design_system/canton_design_system.dart';
 import 'package:corona_spectator/src/config/constants.dart';
 import 'package:corona_spectator/src/ui/views/current_view.dart';
@@ -22,24 +24,32 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() async {
+  runZonedGuarded<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+    await Firebase.initializeApp();
 
-  await Hive.initFlutter();
-  await Hive.openBox('corona_spectator');
+    await Hive.initFlutter();
+    await Hive.openBox('corona_spectator');
+    if (kDebugMode) {
+      await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+    } else {
+      FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+    }
 
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-
-  /// Lock screen orientation to vertical
-  await SystemChrome.setPreferredOrientations(
-      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
-    runApp(ProviderScope(child: MyApp()));
+    /// Lock screen orientation to vertical
+    await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+        .then((_) {
+      runApp(ProviderScope(child: MyApp()));
+    });
+  }, (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack);
   });
 }
 
