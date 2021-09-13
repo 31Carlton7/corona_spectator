c5b55a9ba009acd40b615042beac9c3c36084482
31Carlton7 <carltonaikins7@gmail.com>
2021-09-12 22:40:50 -0400

    Update statistics_view_header.dart

---
+++ lib/src/ui/views/statistics_view/components/statistics_view_header.dart
@@ -0,0 +1,32 @@
+/*
+Copyright (C) 2021  Carlton Aikins
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'package:canton_design_system/canton_design_system.dart';
+
+class StatisticsViewHeader extends StatelessWidget {
+  const StatisticsViewHeader();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 17, left: 17, right: 17),
+      child: ViewHeaderOne(
+        title: 'Statistics',
+      ),
+    );
+  }
+}
