3e5189a625b9a3fdd64a7e3293be2e0ac33b1625
Carlton Aikins <carltonaikins7@gmail.com>
2021-10-18 19:01:00 -0400

    Create nearest_support_locations_view_header.dart

---
+++ lib/src/ui/views/nearest_support_locations_view/components/nearest_support_locations_view_header.dart
@@ -0,0 +1,13 @@
+import 'package:canton_design_system/canton_design_system.dart';
+
+class NearestSupportLocationsViewHeader extends StatelessWidget {
+  const NearestSupportLocationsViewHeader();
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewHeaderTwo(
+      title: 'Nearest Support Locations',
+      backButton: true,
+    );
+  }
+}
